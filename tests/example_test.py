c570aac64f16613dc0ac7bcd42449b51df7a7c6d
Иван Чувахин <chuvakhin@hopper-it.ru>
2024-04-12 14:18:17 +0300

    v6

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,9 +1,11 @@
+import allure
+
 from tests.example_page import ExamplePage
 from web.allure.allure import allure_step
 
 
-# @allure.feature('feature')
-# @allure.story('story')
+@allure.feature('Какая-то фича')
+@allure.story('Какое-то стори')
 def test_title(chrome):
     page = ExamplePage(chrome.page)
     with allure_step("Открыть страницу"):

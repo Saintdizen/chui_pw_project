fd423447a0b13edd91afd7befa49db1abe0800f7
Иван Чувахин <chuvakhin@hopper-it.ru>
2024-05-31 10:48:48 +0300

    test v16

--- web/allure/allure.py
+++ web/allure/allure.py
@@ -19,6 +19,6 @@ def t_step(name):
         try:
             yield
             make_screen("screen")
-        except Exception as e:
+        except Exception:
             make_screen("error")
             raise
